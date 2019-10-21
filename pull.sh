7546c23bc99656b23279361f41cbfa19680f3952
scorpiosixnine <scorpiosixnine@protonmail.com>
2019-10-21 13:31:55 +0100

    updated pull script

--- pull.sh
+++ pull.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+if [[ "$MODULE_NAME" == "" ]]
+then
+  echo "ERROR: Module name not set."
+  exit 1
+fi
+
 DATA="/mnt/d/Steam/steamapps/common/Skyrim Special Edition/Data"
+
+echo "Pulling from Skyrim Data Folder"
 
 mkdir -p Source/Scripts
 mkdir -p Scripts
 mkdir -p Meshes/$MODULE_NAME
 
-cp "$DATA/Source/Scripts/"*$MODULE_NAME* Source/Scripts/
 cp "$DATA/$MODULE_NAME.esp" .
+cp "$DATA/Source/Scripts/"*$MODULE_NAME* Source/Scripts/
+
+if [[ -e "DATA/Meshes/$MODULE_NAME/" ]]
+then
+  cp -r "$DATA/Meshes/$MODULE_NAME" Meshes/
+fi
+
+echo "Done."
