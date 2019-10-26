1513b64ba025364a5df57f290e85fae32c95738e
scorpiosixnine <scorpiosixnine@protonmail.com>
2019-10-26 17:10:26 +0100

    expanded pull to grab bodyslide stuff

--- pull.sh
+++ pull.sh
@@ -19,13 +19,36 @@ echo "Pulling from Skyrim Data Folder"
 mkdir -p Source/Scripts
 mkdir -p Scripts
 mkdir -p Meshes/$MODULE_NAME
+mkdir -p CalienteTools/Bodyslide/SliderSets/
+mkdir -p CalienteTools/Bodyslide/SliderGroups/
+mkdir -p CalienteTools/Bodyslide/ShapeData/$MODULE_NAME/
 
 cp "$DATA/$MODULE_NAME.esp" .
-cp "$DATA/Source/Scripts/"*$MODULE_NAME* Source/Scripts/
 
-if [[ -e "DATA/Meshes/$MODULE_NAME/" ]]
+if [[ -e "$DATA/Source/Scripts/"*$MODULE_NAME* ]]
+then
+  cp "$DATA/Source/Scripts/"*$MODULE_NAME* Source/Scripts/
+fi
+
+if [[ -e "$DATA/Meshes/$MODULE_NAME/" ]]
 then
   cp -r "$DATA/Meshes/$MODULE_NAME" Meshes/
 fi
+
+if [[ -e "$DATA/CalienteTools/Bodyslide/SliderSets/$MODULE_NAME.osp" ]]
+then
+  cp -r "$DATA/CalienteTools/Bodyslide/SliderSets/$MODULE_NAME.osp" "CalienteTools/Bodyslide/SliderSets/"
+fi
+
+if [[ -e "$DATA/CalienteTools/Bodyslide/SliderGroups/$MODULE_NAME.xml" ]]
+then
+  cp -r "$DATA/CalienteTools/Bodyslide/SliderGroups/$MODULE_NAME.xml" "CalienteTools/Bodyslide/SliderGroups/"
+fi
+
+if [[ -e "$DATA/CalienteTools/Bodyslide/ShapeData/$MODULE_NAME/" ]]
+then
+  cp -r "$DATA/CalienteTools/Bodyslide/ShapeData/$MODULE_NAME/"* "CalienteTools/Bodyslide/ShapeData/$MODULE_NAME/"
+fi
+
 
 echo "Done."
