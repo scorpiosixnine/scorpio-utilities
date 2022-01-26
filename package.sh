85b8f89a3eb3230937252cce4798a80497386617
scorpiosixnine <scorpiosixnine@protonmail.com>
2022-01-26 12:02:21 +0000

    Fixed packing script to use .build folder

--- package.sh
+++ package.sh
@@ -1,50 +1,35 @@
 #!/bin/bash
 
+source scorpio-utilities/build.sh
 
-if [[ "$MODULE_NAME" == "" ]]
-then
-  echo "ERROR: Module name not set."
-  exit 1
-fi
-
-if [[ "$SKYRIM_HOME" == "" ]]
-then
-  echo "ERROR: Skyrim location name not set."
-  exit 1
-fi
-
-DATA="$SKYRIM_HOME/Data"
-
+set -x
 
 NAME="$MODULE_NAME-$VERSION-$BUILD_NO"
-FOLDER="Releases/$MODULE_NAME"
+PACKAGE="Releases/$MODULE_NAME"
+MOD="$PACKAGE/$MODULE_NAME"
 ARCHIVE="$NAME.zip"
 
-cp "$DATA/$MODULE_NAME.esp" .
+mkdir -p "$MOD/Source/Scripts"
 
-mkdir -p "$FOLDER/$MODULE_NAME/Source"
+# Copy the esp 
+cp "$MODULE_NAME.esp" "$MOD"
 
-cp "$DATA/$MODULE_NAME.esp" "$FOLDER/$MODULE_NAME/"
-cp -r Source/Scripts "$FOLDER/$MODULE_NAME/Source/Scripts"
+# Copy expanded source
+cp "$SOURCE/"*.psc "$CONTENT/Source/Scripts/"
 
-# grab substituted source directly from the data folder
-cp "$DATA/Source/Scripts/${MODULE_NAME}Quest.psc" "$FOLDER/$MODULE_NAME/Source/Scripts/"
-cp "$DATA/Source/Scripts/${MODULE_NAME}Config.psc" "$FOLDER/$MODULE_NAME/Source/Scripts/"
+# Copy other content
+cp "$CONTENT"/* "$MOD/"
 
-mkdir -p "$FOLDER/$MODULE_NAME/Scripts"
-for f in "$FOLDER/$MODULE_NAME/Source/Scripts/"*
-do
-  filename=$(basename -- "$f")
-  name="${filename%.*}"
-  cp "$DATA/Scripts/$name.pex" "$FOLDER/$MODULE_NAME/Scripts/"
-done
+# Copy compiled scripts
+cp "$OUTPUT/"*.pex "$MOD/Scripts/"
 
-cp -r fomod "$FOLDER/"
-sed -i "s/{VERSION}/$VERSION/g" "$FOLDER/fomod/info.xml"
+# Copy fomod
+cp -r fomod "$PACKAGE/"
+sed -i "s/{VERSION}/$VERSION/g" "$PACKAGE/fomod/info.xml"
 
 rm "Releases/$ARCHIVE"
 cd "Releases/$MODULE_NAME"
 zip -r "../$ARCHIVE" "fomod" "$MODULE_NAME"
 cd ../..
 
-rm -rf "$FOLDER"
+#rm -rf "$PACKAGE"
